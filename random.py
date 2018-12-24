37afb6ec4e70fdf5af7bae92c80cee9223147e11
Madininais <lologoetz@yahoo.fr>
2018-12-24 19:22:47 +0100

    test

---
+++ random.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+rng = np.random.RandomState(0)
+
+np.random.rand(5,2)
+np.random.randn(5,2)
+np.random.randint(low,high)
+np.random.randint(20, size = 10)
+
+np.random.random(5)
+np.random.random((5,2))
+
+np.random.choice(X.shape[0], 20, replace=False)
+
+
+lalalalalal
+bababa
